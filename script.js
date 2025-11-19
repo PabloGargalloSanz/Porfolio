f3c7e4c718e32642eb1b0562a8a4f1ad0532acc3
PabloGargalloSanz <pablogargallosanz@gmail.com>
2025-11-19 23:17:49 +0100

    cambio total

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script para smooth scrolling (desplazamiento suave)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
